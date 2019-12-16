6a7a856c6f928230b382c8ea0b92ba2017cad8ae
Erik Olseng <e.olseng@gmail.com>
2019-12-16 12:15:39 +0100

    Add Product class and first Products

---
+++ js/products.js
@@ -0,0 +1,30 @@
+class Product {
+
+    constructor(brand, category, subCategory, gender, model, color, imageLink) {
+        this.brand = brand;
+        this.category = category;
+        this.subCategory = subCategory;
+        this.gender = gender;
+        this.model = model;
+        this.color = color;
+        this.imageLink = imageLink;
+    }
+
+}
+
+let products = [];
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Male", "Skagen Offshore Jacket", "Red", "https://www.hellyhansen.com/media/catalog/product/3/3/33907_222-2-main.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Male", "Skagen Offshore Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33907_597-2-main.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Skagen Offshore Jacket", "White", "https://www.hellyhansen.com/media/catalog/product/3/3/33920_001-1-hero.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Skagen Offshore Jacket", "Red", "https://www.hellyhansen.com/media/catalog/product/3/3/33920_222-2-main3.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Skagen Offshore Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33920_597-2-main.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Male", "Salt Flag Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33909_603-2-main.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Male", "Salt Flag Jacket", "Black", "https://www.hellyhansen.com/media/catalog/product/3/3/33909_597-1-hero.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Salt Flag Jacket", "White", "https://www.hellyhansen.com/media/catalog/product/3/3/33923_001-1-hero.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Salt Flag Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33923_994-2-main4.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Salt Flag Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33923_994-2-main4.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Salt Flag Jacket", "Blue", "https://www.hellyhansen.com/media/catalog/product/3/3/33923_994-2-main4.jpg?quality=75&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:"));
