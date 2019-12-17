32e33be4d64b93b067b59d407ae4273d29e512fa
Hessh <haakon.hess@gmail.com>
2019-12-17 10:00:19 +0100

    Removed old function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /** SUPPORT FUNCTIONS **/
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
@@ -6,21 +5,3 @@ function getUrlParameter(name) {
     let results = regex.exec(location.search);
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 }
-
-/** FOR PROFILE PAGE */
-let topNav = document.querySelectorAll(".top-nav")[0]
-let topNavButtons = document.querySelectorAll(".top-nav-button")
-
-function toggle(data){
-    for (let i = 0; i < topNavButtons.length; i++) {
-        if(i != data) {
-            topNavButtons[i].style.textDecoration = "none"
-            topNavButtons[i].style.fontWeight = "400"
-        } else {
-            topNavButtons[i].style.textDecoration = "underline"
-            topNavButtons[i].style.fontWeight = "600"
-        }
-    }
-}
-
-toggle(0)
