109d28b3e90a660fd7c6f34ad585b4c64fb4f965
Erik Olseng <e.olseng@gmail.com>
2019-12-17 11:51:07 +0100

    Finished first draft of filter page

--- js/products.js
+++ js/products.js
@@ -72,8 +72,6 @@ products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W C
 products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Pink", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_039-2-main.jpg"));
 products.push(new Product("Jelly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Black", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_990-2-main.jpg"));
 
-
-let filteredProducts = [];
 let selectedBrand;
 let selectedCategory;
 let selectedSubCategory;
@@ -81,6 +79,7 @@ let selectedGender;
 let selectedModel;
 let selectedColor;
 
+let filteredProducts = [];
 let brands = [];
 let categories = [];
 let subCategories = [];
@@ -88,24 +87,7 @@ let genders = [];
 let models = [];
 let colors = [];
 
-products.forEach(b => brands.push(b.brand));
-brands = [...new Set(brands)];
-
-products.forEach(b => categories.push(b.category));
-categories = [...new Set(categories)];
-
-products.forEach(b => subCategories.push(b.subCategory));
-subCategories = [...new Set(subCategories)];
-
-products.forEach(b => genders.push(b.gender));
-genders = [...new Set(genders)];
-
-products.forEach(b => models.push(b.model));
-models = [...new Set(models)];
-
-products.forEach(b => colors.push(b.color));
-colors = [...new Set(colors)];
-
+let productsElement = document.getElementById("products-container");
 let brandElement = document.getElementById("filter-brand");
 let categoryElement = document.getElementById("filter-category");
 let subCategoryElement = document.getElementById("filter-sub-category");
@@ -115,14 +97,15 @@ let modelElement = document.getElementById("filter-model");
 
 function renderFilterPage(){
     clearMenu();
-    renderMenu();
     filter(products);
+    renderMenu();
     renderProducts(filteredProducts);
 }
 
 function renderProducts(products){
+    productsElement.innerHTML = "";
     for (let i = 0; i < products.length; i++) {
-        renderProduct(document.getElementById("products-container"), products[i]);
+        renderProduct(productsElement, products[i]);
     }
 }
 
@@ -136,10 +119,20 @@ function clearMenu() {
 }
 
 function renderMenu(){
-    brands.forEach(brand => {
-        let node = createDivWithContent("filter-element", brand);
+    brands.forEach(element => {
+        let node = createDivWithContent("filter-element", element);
+
+        if(element == selectedBrand) node.classList.add("selected-filter-element");
+
         node.addEventListener("click", function(){
-            selectedBrand = brand;
+            selectedBrand = element;
+
+            selectedCategory = null;
+            selectedSubCategory = null;
+            selectedGender = null;
+            selectedModel = null;
+            selectedColor = null;
+
             renderFilterPage();
         });
         brandElement.appendChild(node);
@@ -149,55 +142,110 @@ function renderMenu(){
 
     categories.forEach(element => {
         let node = createDivWithContent("filter-element", element);
+        if(element == selectedCategory) node.classList.add("selected-filter-element");
         node.addEventListener("click", function(){
             selectedCategory = element;
+
+            selectedSubCategory = null;
+            selectedGender = null;
+            selectedModel = null;
+            selectedColor = null;
+
             renderFilterPage();
         });
         categoryElement.appendChild(node);
+    });
+
+    if (!selectedCategory) return;
+
+    subCategories.forEach(element => {
+        let node = createDivWithContent("filter-element", element);
+        if(element == selectedSubCategory) node.classList.add("selected-filter-element");
+        node.addEventListener("click", function(){
+            selectedSubCategory = element;
+
+            selectedGender = null;
+            selectedModel = null;
+            selectedColor = null;
+
+            renderFilterPage();
+        });
+        subCategoryElement.appendChild(node);
+    });
+
+    if (!selectedSubCategory) return;
+
+    genders.forEach(element => {
+        let node = createDivWithContent("filter-element", element);
+        if(element == selectedGender) node.classList.add("selected-filter-element");
+        node.addEventListener("click", function(){
+            selectedGender = element;
+
+            selectedModel = null;
+            selectedColor = null;
+
+            renderFilterPage();
+        });
+        genderElement.appendChild(node);
+    });
+
+    if (!selectedGender) return;
+
+    colors.forEach(element => {
+        let node = createDivWithContent("filter-element", element);
+        if(element == selectedColor) node.classList.add("selected-filter-element");
+        node.addEventListener("click", function(){
+            selectedColor = element;
+
+            renderFilterPage();
+        });
+        colorElement.appendChild(node);
     });
 }
 
 function filter(products){
     filteredProducts = products;
 
+    brands = [];
+    categories = [];
+    subCategories = [];
+    genders = [];
+    models = [];
+    colors = [];
+
     filteredProducts.forEach(b => brands.push(b.brand));
     brands = [...new Set(brands)];
-
-    if(selectedBrand != null){
+    if(selectedBrand){
         filteredProducts = filteredProducts.filter(e => e.brand == selectedBrand)
     }
+
     filteredProducts.forEach(b => categories.push(b.category));
     categories = [...new Set(categories)];
-
-    if(selectedCategory != null){
+    if(selectedCategory){
         filteredProducts = filteredProducts.filter(e => e.category == selectedCategory)
     }
 
     filteredProducts.forEach(b => subCategories.push(b.subCategory));
     subCategories = [...new Set(subCategories)];
-
-    if(selectedSubCategory != null){
+    if(selectedSubCategory){
         filteredProducts = filteredProducts.filter(e => e.subCategory == selectedSubCategory)
     }
 
     filteredProducts.forEach(b => genders.push(b.gender));
     genders = [...new Set(genders)];
-
-    if(selectedGender != null){
+    if(selectedGender){
         filteredProducts = filteredProducts.filter(e => e.gender == selectedGender)
     }
 
     filteredProducts.forEach(b => colors.push(b.color));
     colors = [...new Set(colors)];
-
-    if(selectedColor != null){
+    if(selectedColor){
         filteredProducts = filteredProducts.filter(e => e.color == selectedColor)
     }
 
     filteredProducts.forEach(b => models.push(b.model));
     models = [...new Set(models)];
-
-    if(selectedModel!= null){
+    if(selectedModel){
         filteredProducts = filteredProducts.filter(e => e.model == selectedModel)
     }
 }
