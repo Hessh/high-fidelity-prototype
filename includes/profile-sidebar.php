eb9425773cf8a8f5ca0af7acf4610bf6c3472ad1
Hessh <haakon.hess@gmail.com>
2019-12-17 07:50:17 +0100

    Testing profile

---
+++ includes/profile-sidebar.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-12">
+        <img src="" alt="Profile picture">
+    </div>
+</div>
+<div class="row">
+    <div class="col-12">
+        <h2>Name</h2>
+        <h3>Email</h3>
+        <h3>Address</h3>
+        <button>Edit</button>
+    </div>
+</div>
---
+++ js/script.js
@@ -0,0 +1,16 @@
+let topNav = document.querySelectorAll(".top-nav")[0]
+let topNavButtons = document.querySelectorAll(".top-nav-button")
+
+function toggle(data){
+    for (let i = 0; i < topNavButtons.length; i++) {
+        if(i != data) {
+            topNavButtons[i].style.textDecoration = "none"
+            topNavButtons[i].style.fontWeight = "400"
+        } else {
+            topNavButtons[i].style.textDecoration = "underline"
+            topNavButtons[i].style.fontWeight = "600"
+        }
+    }
+}
+
+toggle(0)
