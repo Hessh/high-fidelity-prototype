c6415f5d8d07edd2ca95feea56d5d30ae0cf6d57
Truls <trulsistre@gmail.com>
2019-12-17 13:20:56 +0100

    added more test products

--- js/products.js
+++ js/products.js
@@ -69,4 +69,13 @@ products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Male", "Crew 
 
 products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Red", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_222-2-main.jpg"));
 products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Pink", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_039-2-main.jpg"));
-products.push(new Product("Jelly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Black", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_990-2-main.jpg"));
+products.push(new Product("Helly Hansen", "Jakke", "Seilerjakke", "Female", "W Crew Midlayer Jacket", "Black", "https://www.hellyhansen.com/media/catalog/product/3/0/30317_990-2-main.jpg"));
+
+products.push(new Product("Norrøna", "Bukse", "Gore-tex", "Male", "Lofoten ACE Gore-Tex Pro", "Mercury", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1009178915.jpg"));
+products.push(new Product("Norrøna", "Bukse", "Gore-tex", "Male", "Lofoten ACE Gore-Tex Pro", "Black", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1002197718.jpg"));
+products.push(new Product("Norrøna", "Bukse", "Gore-tex", "Male", "Lofoten Gore-Tex Pro Light", "Green", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1022173440.jpg"));
+products.push(new Product("Norrøna", "Bukse", "flex™1", "Male", "Falketind flex1 Pants", "Indigo", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/3321142319.jpg"));
+products.push(new Product("Norrøna", "Bukse", "Gore-tex", "Female", "Falketind Gore-Tex Pro Light", "Red", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1324,h_1324,q_auto/v1561713745/1058175700-model-1.jpg"));
+products.push(new Product("Norrøna", "Jakke", "Gore-Tex", "Male", "Lofoten Gore-Tex Pro Jacket", "Green", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1015173440.jpg"));
+products.push(new Product("Norrøna", "Jakke", "Gore-Tex", "Female", "Lofoten Gore-Tex Pro Jacket", "Red", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1054175700.jpg"));
+products.push(new Product("Norrøna", "Jakke", "Gore-Tex", "Female", "Lofoten Gore-Tex Insulated", "Gray", "https://res.cloudinary.com/norrona/image/upload/b_rgb:F5F5F5,c_pad,d_imgmissing.jpg,f_auto,fl_progressive.lossy,w_1000,h_1000,q_auto/1007188874.jpg"));
