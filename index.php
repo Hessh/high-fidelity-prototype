10b1aa2eb360c33bce1de0d6d1a7358d0f5cac1f
maricathrinekarlsen <nerdmary1@gmail.com>
2019-12-18 12:55:13 +0100

    index beuty-design

--- index.php
+++ index.php
@@ -19,11 +19,12 @@
 
 <body>
     <?php include './includes/header.php' ?>
-    <div class="container-fluid first first-index">
-        <div class="container">
+    <div class="container-fluid first-index hero">
+        <div class="container index-container">
             <!-- HEADING -->
             <div class="row">
                 <div class="col-12 main-title-container">
+                    <h1>Velkommen<h1>
                 </div>
             </div>
             <!-- TEXTCONTENT 1 -->
@@ -34,150 +35,149 @@
                         Spar tiden du ellers ville brukt på å undersøke priser, beskrive dine plagg og avtale møtested med kjøper. Vi gjør jobben for deg!</p>
                     <a class="center" href="filter.php"><button class="cta-button">Kom igang nå</button></a>
                 </div>
-                <div class="col-6 img-container">
-                    <img class="right" src="img/browse.jpg" alt="Brukte klær">
-                </div>
+                <div class="col-push-6"></div>
             </div>
         </div>
-        <div class="container-fluid blue-index">
-            <div class="container">
-                <!-- ICON GROUP 1 -->
-                <div class="row">
-                    <div class="col-12 icon-title">
-                        <h2>Slik fungerer det</h2>
-                    </div>
-                </div>
-                <div class="row icon-wrapper">
-                    <div class="col-4 icon-container">
-                        <img src="img/register.png" alt="Icon">
-                        <h3>1. Registrer plagget</h3>
-                        <p>Registrer plagget ved å klikke deg frem til riktig plagg</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/value.png" alt="Icon">
-                        <h3>2. Estimert verdi</h3>
-                        <p>Få en estimert verdi på et gavekort hos gjeldende merke. Slik at du får et inntrykk av plaggets verdi og hva gavekortet du får vil ligge på i verdi</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/ship.png" alt="Icon">
-                        <h3>3. Send/lever</h3>
-                        <p>Motta en sendingspose i posten, pakk, forsegl og send den inn til oss / Lever det i din nærmeste butikk.</p>
-                    </div>
-                </div>
-                <div class="row icon-wrapper">
-                    <div class="col-push-2"></div>
-                    <div class="col-4 icon-container">
-                        <img src="img/wait.png" alt="Icon">
-                        <h3>4. Vent på vurdering</h3>
-                        <p>Vent på verdivurdering fra våre eksperter (3 dager)- aksepter eller avslå tilbudet</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/offer.png" alt="Icon">
-                        <h3>5. Motta tilbud</h3>
-                        <p>Aksepter for å motta gavekort som du kan bruke i gjeldende butikk</p>
-                    </div>
-                    <div class="col-push-2"></div>
-                </div>
-                <!-- CTA BUTTON -->
-                <div class="row">
-                    <div class="col-12 button-container">
-                        <a href="filter.php"><button class="cta-button">Kom igang nå</button></a>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+    <div class="container-fluid blue-index">
         <div class="container">
-            <!-- TEXTCONTENT 2-->
-            <div class="row text-wrapper">
-                <div class="col-6 img-container">
-                    <img src="img/resale.jpg" alt="Gavekort">
-                </div>
-                <div class="col-6 text-container">
-                    <h2 class="title">Hvorfor ReStore?</h2>
-                    <p class="text">Restore betyr å restaurere.
-                        <br>Vi følger definisjonene på å restaurere:
-                    </p>
-                    <p class="text">: å returnere noe
-                        <br>: å bringe noe tilbake til eksistens eller bruk.
-                        <br>: å returnere noe til en tidligere eller original tilstand ved å reparere det, rengjøre det, sette i stand osv.
+            <!-- ICON GROUP 1 -->
+            <div class="row">
+                <div class="col-12 icon-title">
+                    <h2>Slik fungerer det</h2>
                 </div>
             </div>
-            <!-- TEXTCONTENT 3 -->
-            <br><br>
-            <div class="row text-wrapper">
-                <div class="col-6 text-container">
-                    <h2 class="title">Vårt mål</h2>
-                    <p class="text">Vårt mål er å skape en kultur for kjøp og salg av gjenbruksklær. Det vil vi gjøre ved å oppfordre deg som bruker til å sende inn plagg til oss. </p>
-                    <p class="text">Vi sender brukte klær i god stand til butikkenes merker slik at disse kan bli solgt på nytt i deres bruktavdelinger. Samtidig som du bidrar til et
-                        bedre miljø vil du kunne få muligheten til å kjøpe noe nytt med gavekortene du får. Slik minsker vi på sikt store mengder av klesforbruket.</p>
+            <div class="row icon-wrapper">
+                <div class="col-4 icon-container">
+                    <img src="img/register.png" alt="Icon">
+                    <h3>1. Registrer plagget</h3>
+                    <p>Registrer plagget ved å klikke deg frem til riktig plagg</p>
                 </div>
-                <div class="col-6 img-container">
-                    <img class="right" src="img/plant.jpg" alt="Plante">
+                <div class="col-4 icon-container">
+                    <img src="img/value.png" alt="Icon">
+                    <h3>2. Estimert verdi</h3>
+                    <p>Få en estimert verdi på et gavekort hos gjeldende merke. Slik at du får et inntrykk av plaggets verdi og hva gavekortet du får vil ligge på i verdi</p>
                 </div>
-            </div>
-            <!-- TEXTCONTENT 4 -->
-            <div class="row text-wrapper">
-                <div class="col-6 img-container">
-                    <img src="img/thankyou.jpg" alt="Gavekort">
-                </div>
-                <div class="col-6 text-container">
-                    <h2 class="title">Bli med oss å skape kulturen</h2>
-                    <p class="text">Vi ønsker å skape en kultur for kjøp og salg av gjenbruksklær til nye høyder,
-                        ved å gi deg som bruker en godtgjørelse for ditt bidrag og samtidig som du bidrar til et bedre miljø!
-                    </p>
-                    <p class="text">Som takk for at du sender inn dine brukte klær til oss får du ulike gavekort basert på hvilke type merker det er og plaggets tilstand. Derfor har vi også en estimert verdi du får
-                        før du leverer inn, slik at du kan se hva slags verdi dine plagg ligger på.</p>
-                    <a class="center" href="filter.php"><button class="cta-button">Kom igang nå</button></a>
+                <div class="col-4 icon-container">
+                    <img src="img/ship.png" alt="Icon">
+                    <h3>3. Send/lever</h3>
+                    <p>Motta en sendingspose i posten, pakk, forsegl og send den inn til oss / Lever det i din nærmeste butikk.</p>
                 </div>
             </div>
-        </div>
-        <div class="container-fluid blue-index">
-            <div class="container">
-                <!-- ICON GROUP 2 -->
-                <div class="row">
-                    <div class="col-12 icon-title">
-                        <h2>Våre hjertesaker</h2>
-                    </div>
+            <div class="row icon-wrapper">
+                <div class="col-push-2"></div>
+                <div class="col-4 icon-container">
+                    <img src="img/wait.png" alt="Icon">
+                    <h3>4. Vent på vurdering</h3>
+                    <p>Vent på verdivurdering fra våre eksperter (3 dager)- aksepter eller avslå tilbudet</p>
                 </div>
-                <div class="row icon-wrapper">
-                    <div class="col-4 icon-container">
-                        <img src="img/bird.png" alt="Icon">
-                        <h3>Dyr</h3>
-                        <p>Forklarende tekst</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/wallet.png" alt="Icon">
-                        <h3>Din lommebok</h3>
-                        <p>Forklarende tekst</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/ocean.png" alt="Icon">
-                        <h3>Havet</h3>
-                        <p>Forklarende tekst</p>
-                    </div>
+                <div class="col-4 icon-container">
+                    <img src="img/offer.png" alt="Icon">
+                    <h3>5. Motta tilbud</h3>
+                    <p>Aksepter for å motta gavekort som du kan bruke i gjeldende butikk</p>
                 </div>
-                <div class="row icon-wrapper">
-                    <div class="col-push-2"></div>
-                    <div class="col-4 icon-container">
-                        <img src="img/environment.png" alt="Icon">
-                        <h3>Miljø</h3>
-                        <p>Forklarende tekst</p>
-                    </div>
-                    <div class="col-4 icon-container">
-                        <img src="img/trees.png" alt="Icon">
-                        <h3>Regnskogen</h3>
-                        <p>Forklarende tekst</p>
-                    </div>
-                    <div class="col-push-2"></div>
-                </div>
-                <!-- CTA BUTTON -->
-                <div class="row">
-                    <div class="col-12 button-container">
-                        <a href="filter.php"><button class="cta-button">Kom igang nå</button></a>
-                    </div>
+                <div class="col-push-2"></div>
+            </div>
+            <!-- CTA BUTTON -->
+            <div class="row">
+                <div class="col-12 button-container">
+                    <a href="filter.php"><button class="cta-button">Kom igang nå</button></a>
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container">
+        <!-- TEXTCONTENT 2-->
+        <div class="row text-wrapper">
+            <div class="col-6 img-container">
+                <img src="img/resale.jpg" alt="Gavekort">
+            </div>
+            <div class="col-6 text-container">
+                <h2 class="title">Hvorfor ReStore?</h2>
+                <p class="text">Restore betyr å restaurere.
+                    <br>Vi følger definisjonene på å restaurere:
+                </p>
+                <p class="text">: å returnere noe
+                    <br>: å bringe noe tilbake til eksistens eller bruk.
+                    <br>: å returnere noe til en tidligere eller original tilstand ved å reparere det, rengjøre det, sette i stand osv.
+            </div>
+        </div>
+        <!-- TEXTCONTENT 3 -->
+        <br><br>
+        <div class="row text-wrapper">
+            <div class="col-6 text-container">
+                <h2 class="title">Vårt mål</h2>
+                <p class="text">Vårt mål er å skape en kultur for kjøp og salg av gjenbruksklær. Det vil vi gjøre ved å oppfordre deg som bruker til å sende inn plagg til oss. </p>
+                <p class="text">Vi sender brukte klær i god stand til butikkenes merker slik at disse kan bli solgt på nytt i deres bruktavdelinger. Samtidig som du bidrar til et
+                    bedre miljø vil du kunne få muligheten til å kjøpe noe nytt med gavekortene du får. Slik minsker vi på sikt store mengder av klesforbruket.</p>
+            </div>
+            <div class="col-6 img-container">
+                <img class="right" src="img/plant.jpg" alt="Plante">
+            </div>
+        </div>
+        <!-- TEXTCONTENT 4 -->
+        <div class="row text-wrapper">
+            <div class="col-6 img-container">
+                <img src="img/thankyou.jpg" alt="Gavekort">
+            </div>
+            <div class="col-6 text-container">
+                <h2 class="title">Bli med oss å skape kulturen</h2>
+                <p class="text">Vi ønsker å skape en kultur for kjøp og salg av gjenbruksklær til nye høyder,
+                    ved å gi deg som bruker en godtgjørelse for ditt bidrag og samtidig som du bidrar til et bedre miljø!
+                </p>
+                <p class="text">Som takk for at du sender inn dine brukte klær til oss får du ulike gavekort basert på hvilke type merker det er og plaggets tilstand. Derfor har vi også en estimert verdi du får
+                    før du leverer inn, slik at du kan se hva slags verdi dine plagg ligger på.</p>
+                <a class="center" href="filter.php"><button class="cta-button">Kom igang nå</button></a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid blue-index">
+        <div class="container">
+            <!-- ICON GROUP 2 -->
+            <div class="row">
+                <div class="col-12 icon-title">
+                    <h2>Våre hjertesaker</h2>
+                </div>
+            </div>
+            <div class="row icon-wrapper">
+                <div class="col-4 icon-container">
+                    <img src="img/bird.png" alt="Icon">
+                    <h3>Dyr</h3>
+                    <p>Forklarende tekst</p>
+                </div>
+                <div class="col-4 icon-container">
+                    <img src="img/wallet.png" alt="Icon">
+                    <h3>Din lommebok</h3>
+                    <p>Forklarende tekst</p>
+                </div>
+                <div class="col-4 icon-container">
+                    <img src="img/ocean.png" alt="Icon">
+                    <h3>Havet</h3>
+                    <p>Forklarende tekst</p>
+                </div>
+            </div>
+            <div class="row icon-wrapper">
+                <div class="col-push-2"></div>
+                <div class="col-4 icon-container">
+                    <img src="img/environment.png" alt="Icon">
+                    <h3>Miljø</h3>
+                    <p>Forklarende tekst</p>
+                </div>
+                <div class="col-4 icon-container">
+                    <img src="img/trees.png" alt="Icon">
+                    <h3>Regnskogen</h3>
+                    <p>Forklarende tekst</p>
+                </div>
+                <div class="col-push-2"></div>
+            </div>
+            <!-- CTA BUTTON -->
+            <div class="row">
+                <div class="col-12 button-container">
+                    <a href="filter.php"><button class="cta-button">Kom igang nå</button></a>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
     <?php include './includes/footer.php' ?>
     <script src="js/menu.js"></script>
