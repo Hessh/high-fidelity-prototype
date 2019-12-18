b5ac7ad0fb862e904f163ab40843c13a97d013b9
maricathrinekarlsen <nerdmary1@gmail.com>
2019-12-18 15:02:41 +0100

    profilbilde

--- includes/profile-sidebar.php
+++ includes/profile-sidebar.php
@@ -1,24 +1,23 @@
-/*<div class="row">
-    <div class="col-12">
-        <div class="profile-picture-container">
-            <img src="img/man-profile.jpg" alt="Profile picture">
-        </div>
+<div class="row icon-wrapper">
+    <div class="icon-container">
+        <img class="col-8" src="img/man-profile.jpg" alt="Profilbilde">
     </div>
 </div>
+
 <div class="row">
     <div class="col-12">
         <h2>Ola Nordmann</h2>
         <div class="info-wrapper">
             <span class="material-icons-outlined">email</span>
-            <span>ola-gutten1@gmail.com</span>
+            <span>ola-gutt1@gmail.com</span>
         </div>
         <div class="info-wrapper">
             <span class="material-icons-outlined">room</span>
             <span>Heiveien 1,</span>
             <div class="last-span">
-                <span>0570 Oslo</span>
+                <span>0570, Oslo</span>
             </div>
         </div>
-        <button id="edit-button" class="edit-button" onclick="editProfile()">Edit Profile</button>
+        <button id="edit-button" class="edit-button" onclick="editProfile()">Endre profil</button>
     </div>
 </div>
