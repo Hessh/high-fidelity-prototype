39e0a73b163576e42b05ed283f2b0b48fb1b584c
Truls <trulsistre@gmail.com>
2019-12-19 09:20:39 +0100

    added numbers on the progressbar

--- includes/progress.php
+++ includes/progress.php
@@ -11,21 +11,21 @@
 
 
     <div class="progress-row" id="progress-one-container">
-        <div class="progress-text" id="progress-one">Finn produkt</div>
+        <div class="progress-text" id="progress-one">1. Finn produkt</div>
         <div id="circle-one" class="progress-circle">
             <img src="img/search.svg" id="product">
         </div>
     </div>
 
     <div class="progress-row" id="progress-two-container">
-        <div class="progress-text" id="progress-two">Vurder tilstand</div>
+        <div class="progress-text" id="progress-two">2. Vurder tilstand</div>
         <div id="circle-two" class="progress-circle">
             <img src="img/book.svg" id="condition">
         </div>
 
     </div>
     <div class="progress-row" id="progress-four-container">
-        <div class="progress-text" id="progress-four">Velg leveringsmetode</div>
+        <div class="progress-text" id="progress-four">3.Velg leveringsmetode</div>
         <div id="circle-four" class="progress-circle">
             <img src="img/deliver.svg" id="delivery">
         </div>
@@ -33,7 +33,7 @@
     </div>
 
     <div class="progress-row" id="progress-five-container">
-        <div class="progress-text" id="progress-five">Få bekreftelse</div>
+        <div class="progress-text" id="progress-five">4. Få bekreftelse</div>
         <div id="circle-five" class="progress-circle">
             <img src="img/ship.png" id="done">
         </div>
